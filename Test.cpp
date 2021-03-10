d843ac7d05dfe648904eae826d0556dd64146785
Moti Dahari <motidaharii@gmail.com>
2021-03-10 19:36:02 +0200

    upload

--- Test.cpp
+++ Test.cpp
@@ -1,119 +1,92 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+ /**
+  * Snowman Tests
+  * CPP course, Ariel University
+  * For the full documentation visit: https://codegolf.stackexchange.com/questions/49671/         do-you-want-to-code-a-snowman
+
+  * Created by: Moti Dahari
+  * https://github.com/motidahari
+  */
+ 
+
+// #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include <stdlib.h> 
 #include "snowman.hpp"
 #include <iostream>
 #include <string>
-using namespace std;
+#include <algorithm>
+using namespace std; 
+using namespace ariel;
 
 
-
-
-
-int[] arrayNum = {
-  11111111,
-  21111111,
-  31111111,
-  41111111,
-  42111111,
-  43111111
-  };
-
-string[] arrayPrint = {
-  " _===_\n (.,.)\n<( : )>\n ( : )",
-  "  ___\n .....\n (.,.)\n<( : )>\n ( : )\n",
-  "   _\n   /_\\n (.,.)\n<( : )>\n ( : )\n",
-  "  ___ \n (_*_)\n (.,.)\n<( : )>\n ( : )\n ",
-  "  ___\n (_*_)\n (o,.)\n<( : )>\n ( : )\n",
-  "  ___\n (_*_)\n (O,.)\n<( : )>\n ( : )\n"
-};
-
-
-int getRandomNum(int size) {//1-4
-    int number = 0;
-    for (size_t i = 0; i < size; i++){
-        number *= 10;
-        number += (rand() % 4 + 1);//generate number between 1 and 10:
-    }
-    return number;
+/**
+ * Returns the input string without the whitespace characters: space, newline and tab.
+ * Requires std=c++2a.
+ */
+string nospaces(string str){
+  str.erase(remove(str.begin(), str.end(), ' '), str.end());
+  str.erase(remove(str.begin(), str.end(), '\t'), str.end());
+  str.erase(remove(str.begin(), str.end(), '\n'), str.end());
+  str.erase(remove(str.begin(), str.end(), '\r'), str.end());
+  return str;
 }
-int getRandomNum2(int size) {//1-10
-    int number = 0;
-    for (size_t i = 0; i < size; i++){
-        number *= 10;
-        number += (rand() % 4 + 1);//generate number between 1 and 10:
-    }
-    return number;
+
+
+/**
+ * This test runs on an array of CorrectNumbers (whose range is not 1-4)
+ */
+TEST_CASE("Check Correct Numbers"){
+    CHECK(nospaces(snowman(13231112)) == nospaces(" _===_ (o_O) <( : )> (" ")"));
+    CHECK(nospaces(snowman(11114411)) == nospaces("  _===_ (.,.)  ( : )  ( : )"));
+    CHECK(nospaces(snowman(33232124)) == nospaces("  ___  /_\\(o_O)  (] [)> (   )"));
+    CHECK(nospaces(snowman(12222212)) == nospaces("  _===_\\(o.o)/ ( : )  (" ")"));
+    CHECK(nospaces(snowman(42232124)) == nospaces("   _ (_*_)\\(o.O)  (] [)> (   )"));
+    CHECK(nospaces(snowman(32443333)) == nospaces("  ___  /_\\ (-.-) /(> <)\\ (___)"));
+    CHECK(nospaces(snowman(44444432)) == nospaces("   _(_*_) (- -)  (> <)  (" ")"));
+    CHECK(nospaces(snowman(44114432)) == nospaces("   _ (_*_) (. .)  (> <)  (" ")"));
 }
-int checkSize(int x){
-    std::string text = "";
-    text += std::to_string(x);
-    return (text.size() == 8) ? 1 : -1;
-}
-int checkValue(int x){
-  
-  std::string text = "";
-  text += std::to_string(x);
-  if(text.size() != 8){
-    return -1;
+
+/**
+ * This test runs on an array of IncorrectNumbers (whose range is not 1-4)
+ */
+TEST_CASE("Check Incorrect Numbers"){
+  int size = sizeof(IncorrectNumbers)/sizeof(IncorrectNumbers[0]);
+  for (size_t i = 0; i < size; i++){
+    CHECK_THROWS(snowman(IncorrectNumbers[i]));
   }
-  for (size_t i = 0; i < text.size(); i++){
-    if (text[i] < 49 || text[i] > 52){
-      return -1;
-    }
+}
+
+
+
+/**
+ * This test runs on an array of NegativeNumbers and check the function snowman
+ */
+TEST_CASE("Check Negative Numbers"){
+  int size = sizeof(NegativeNumbers)/sizeof(NegativeNumbers[0]);
+  for (size_t i = 0; i < size; i++){
+    CHECK_THROWS(snowman(NegativeNumbers[i]));
   }
-  return 1;
-}
-
-// TEST_CASE("testing the length") {
-//     for (size_t i = 0; i < 10; i++){
-//         int a = getRandomNum(i);
-//         bool FLAG = checkSize(a);
-//         if(i != 8){
-//             CHECK(FLAG == false);
-//         }else{
-//             CHECK(FLAG == true);
-//         }
-//     }
-// }
-
-
-// TEST_CASE("testing the value") {
-//     for (size_t i = 0; i < 100; i++){
-//       int num = getRandomNum2(i);
-//       int flag = checkSize(num);
-//       if(flag == 1){
-//         CHECK(flag == 1);
-//       }else{
-//         CHECK(flag == -1);
-//       }
-//     }
-// }
-
-
-TEST_CASE("testing the result of snowMan") {
-  int size = (sizeof(arrayNum)/sizeof(*arrayNum));
-    for (size_t i = 0; i < size; i++){
-      int flag = checkSize(num); 
-      if(flag == 1){
-        cout << "checkSize = " << flag <<"\n";
-        cout << "a = " << a <<"\n";
-        cout << "checkValue = " << checkValue(a) <<"\n\n";
-        CHECK(flag == 1);
-        CHECK(nospaces(snowman(arrayNum[i])) == nospaces(arrayPrint[i]));
-
-      }else{
-        cout << "checkSize = " << flag <<"\n";
-        cout << "a = " << a <<"\n";
-        cout << "checkValue = " << checkValue(a) <<"\n\n";
-        CHECK(nospaces(snowman(arrayNum[i])) == nospaces(arrayPrint[i]));
-      }
-    }
 }
 
 
+/**
+ * This test runs on an array of smallerThan8Characters and check the function snowman
+ */
+TEST_CASE("Check smaller Than 8 Characters Numbers"){
+  int size = sizeof(smallerThan8Characters)/sizeof(smallerThan8Characters[0]);
+  for (size_t i = 0; i < size; i++){
+    CHECK_THROWS(snowman(smallerThan8Characters[i]));
+  }
+}
 
 
-
-
+/**
+ * This test runs on an array of biggerThan8Characters and check the function snowman
+ */
+TEST_CASE("Check bigger Than 8 Characters Numbers"){
+  int size = sizeof(biggerThan8Characters)/sizeof(biggerThan8Characters[0]);
+  for (size_t i = 0; i < size; i++){
+    CHECK_THROWS(snowman(biggerThan8Characters[i]));
+  }
+}
 
--- snowman.cpp
+++ snowman.cpp
@@ -1,20 +1,41 @@
+ /**
+  * Snowman
+  * Create a snowman by getting a positive number with 8 digits in the range of 1-4
+  * CPP course, Ariel University
+  * For more documentation visit: 
+  * https://codegolf.stackexchange.com/questions/49671/do-you-want-to-code-a-snowman
+  * Created by: Moti Dahari
+  * https://github.com/motidahari
+  */
+ 
 #include <iostream>
 #include <string>
-#include <iostream>
+// #include <iostream>
 #include <stdexcept>
 #include "snowman.hpp"
+#include <regex>
+#include <algorithm>
 
 using namespace std;
+//Proper length of the number
+const int ProperNumberOfCharacters = 8;
 
-string Hat[4] = {" _===_\n", "  ___\n .....\n", "   _ \n  /_\\\n", "  ___ \n (_*_)\n"};
-string LeftEye[4] = {".", "o", "O", "-"};
-string Mouth[4] = {",", ".", "_", " "};
-string RightEye[4] = {".", "o", "O", "-"};
-string LeftArm[5] = {"<", "\\", "/", " ", "\\"};
-string Torso[4] = {" : ", "] [", "> <", "   "};
-string RightArm[5] = {">", "/", "\\", " ", "/"};
-string Base[4] = {" ( : )", " (   )", " (___)", " (   )"};
+//arrays that represents a string of parts of the snowman's body
+const string Hat[4] = {" _===_\n", "  ___\n .....\n", "   _ \n  /_\\\n", "  ___ \n (_*_)\n"};
+const string LeftEye[4] = {".", "o", "O", "-"};
+const string Mouth[4] = {",", ".", "_", " "};
+const string RightEye[4] = {".", "o", "O", "-"};
+const string LeftArm[5] = {"<", "\\", "/", " ", "\\"};
+const string Torso[4] = {" : ", "] [", "> <", "   "};
+const string RightArm[5] = {">", "/", "\\", " ", "/"};
+const string Base[4] = {" ( : )", " (   )", " (___)", " (   )"};
 
+/**
+ * getIndex - Gets a char which represents a number (from ascii table) and returns its numeric value less 1
+ * For example: if we get "1" we return 0, if we get "2" we return 1..
+ * @param: c - char which represents a number
+ * @return: index - The value from the ascii table is less than 1
+ */
 int getIndex(char c){
   int ascii = c;
   if (ascii >= 49 && ascii <= 52){
@@ -35,193 +56,160 @@ int getIndex(char c){
   }
   return -1;
 }
-string snowman(int num){
-  std::string str = "";
-  str += std::to_string(num);
-  string result = "";
 
-  int UpLeftArm = (str[4] == '2') ? 1 : 0;
-  int UpRightArm = (str[5] == '2') ? 1 : 0;
 
-  for (size_t i = 0; i < str.size(); i++){
-    int index = getIndex(str[i]);
-    if (i == 0){
-      result += Hat[index];
-    }
-    else if (i == 1){
-      if (UpLeftArm == 1){
-        result += LeftArm[4] + "(" + LeftEye[index];
-      }else{
-       result += " (" + LeftEye[index];
-      }
-    }
-    else if (i == 2){
-      result += Mouth[index];
-    }else if (i == 3) {
-      if (UpRightArm == 1){
-        result += RightEye[index] + ")" + RightArm[4] + "\n";
-      }else{
-        result += RightEye[index] + ")\n";
-      }
-    }
-    else if (i == 4){
-      if (UpLeftArm != 1){
-        result += LeftArm[index];
-      }else{
-        result += " ";
-      }
-    }
-    else if (i == 5){
-      if (UpRightArm != 1){
-        result += "(" + Torso[index] + ")";
-      }else{
-        result += "(" + Torso[index] + ")\n";
-      }
-    }else if (i == 6){
-      if (UpRightArm != 1){
-        result += RightArm[index] + "\n";
-      }
-    }
-    else if (i == 7){
-      result += Base[index];
-    }
-  }
-  return result;  
-}
-
-void printSnowMan(string str){
-  int UpLeftArm = (str[4] == '2') ? 1 : 0;
-  int UpRightArm = (str[5] == '2') ? 1 : 0;
-  // cout << "UpLeftArm = " << UpLeftArm << "\n";
-  // cout << "UpRightArm = " << UpRightArm <<  "\n";
-
-  for (size_t i = 0; i < str.size(); i++){
-    int index = getIndex(str[i]);
-    if (i == 0){
-      cout << Hat[index];
-    }
-    else if (i == 1){
-      if (UpLeftArm == 1){
-        cout << LeftArm[4] << "(";
-        cout << LeftEye[index];
-      }else{
-        cout << " (" << LeftEye[index];
-      }
-    }
-    else if (i == 2){
-      cout << Mouth[index];
-    }else if (i == 3) {
-      if (UpRightArm == 1){
-        cout << RightEye[index] << ")" << RightArm[4] << "\n";
-      }else{
-        cout << RightEye[index] << ")\n";
-      }
-    }
-    else if (i == 4){
-      if (UpLeftArm != 1){
-        cout << LeftArm[index];
-      }else{
-        cout << " ";
-      }
-    }
-    else if (i == 5){
-      // cout << "(" << Torso[index] << ")";
-      if (UpRightArm != 1){
-        cout << "(" << Torso[index] << ")";
-      }else{
-        cout << "(" << Torso[index] << ")\n";
-      }
-    }else if (i == 6){
-      // if (UpRightArm != 1){
-      //   cout << RightArm[index] << "\n";
-      // }
-      if (UpRightArm != 1){
-        cout << RightArm[index] << "\n";
-      }
-    }
-    else if (i == 7){
-      cout << Base[index];
-    }
+/**
+ * checkNegativeNumber - Check whether the number is a negative number or not
+ * @param: num - A number
+ * @return: 1 - if it is a positive number, -1 if isn't.
+ */
+int checkNegativeNumber(int num){
+  if(num <= 0){
+    throw invalid_argument{"Error: The number is a negative number\n"};
+    return -1;
+  }else{
+    return 1;
   }
 }
 
-bool checkValue(string x){
-  if(x.size() != 8){
-    return false;
+/**
+ * checkSize - Check whether the string size is 8
+ * @param: x - A String
+ * @return: 1 - If the string length is exactly 8, -1 if isn't.
+ */
+int checkSize(string x){
+  if(x.size() > ProperNumberOfCharacters){
+    throw invalid_argument{"Error: The number of characters is greater than 8\n"};
+    return -1;
+  }else if(x.size() < ProperNumberOfCharacters){
+    throw invalid_argument{"Error: The number of characters is less than 8\n"};
+    return -1;
   }
-  bool flag = true;
+  return 1;
+}
+
+/**
+ * checkValue - Check if all the characters in the string are in the range of 1-4
+ * @param: x - A String
+ * @return: 1 - If the string is really in the 1-4 range, -1 if isn't.
+ */
+int checkValue(string x){
   for (size_t i = 0; i < x.size(); i++){
     if (x[i] < 49 || x[i] > 52){
-      return false;
+      throw invalid_argument{"Error: The input is not in the 1-4 range\n"};
+      return -1;
     }
   }
-  return true;
+  return 1;
 }
 
-int main(){
-  const char *arr[52]  = {
-    "11111111",
-    "21111111",
-    "31111111",
-    "41111111",
-    "42111111",
-    "43111111",
-    "44111111",
-    "44211111",
-    "44311111",
-    "44411111",
-    "44421111",
-    "44431111",
-    "44441111",
-    "44441111",
-    "44442111",
-    "44443111",
-    "44444111",
-    "44444211",
-    "44444311",
-    "44444411",
-    "44444421",
-    "44444431",
-    "44444441",
-    "44444442",
-    "44444443",
-    "11111111",
-    "11111112",
-    "11111113",
-    "11111114",
-    "11111114",
-    "11111124",
-    "11111134",
-    "11111144",
-    "11111244",
-    "11111344",
-    "11111444",
-    "11112444",
-    "11113444",
-    "11114444",
-    "11124444",
-    "11134444",
-    "11144444",
-    "11244444",
-    "11344444",
-    "11444444",
-    "12444444",
-    "13444444",
-    "14444444",
-    "24444444",
-    "34444444",
-    "44444444",
-    "44444444"
-  };
-  int size = (sizeof(arr)/sizeof(*arr));
-   for (int i = 0; i < size; i++){
-    if (checkValue(arr[i]) == true){
-      cout << "\nx = " << arr[i] << "\n";
-      printSnowMan(arr[i]);
-    }else{
-      cout << "error value";
+
+/**
+ * getRandomNum - Produces a random number in the range 1-4
+ * @param: size - The number of characters in the number
+ * @return: number - Positive number
+ */
+int getRandomNum(int size) {//1-4
+    int number = 0;
+    for (size_t i = 0; i < size; i++){
+        number *= 10;
+        number += (rand() % 4 + 1);//generate number between 1 and 4:
     }
-   }
-  
-return 0;
+    return number;
 }
+
+/**
+ * getRandomNumFrom1To10 - Produces a random number in the range 1-10
+ * @param: size - The number of characters in the number
+ * @return: number - Positive number
+ */
+int getRandomNumFrom1To10(int size) {//1-10
+    int number = 0;
+    for (size_t i = 0; i < size; i++){
+        number *= 10;
+        number += (rand() % 4 + 1);//generate number between 1 and 10:
+    }
+    return number;
+}
+
+namespace ariel{
+  /**
+   * snowman - A function that receives a number, checks whether the input is correct, if the input is 
+   * incorrect it throws an error, if the input is correct it returns a string representing a snowman 
+   * which is constructed by each character in the string
+   * @param: num - A number
+   * @return: snowMan - A string representing a snowman, empty string and trow error if isn't.
+   */
+  string snowman(int num){
+    string str = std::to_string(num);
+    string result = "";
+
+    if(checkNegativeNumber(num) != -1 && checkValue(str) != -1 && checkSize(str) != -1){
+      int UpLeftArm = (str[4] == '2') ? 1 : 0;
+      int UpRightArm = (str[5] == '2') ? 1 : 0;
+      for (size_t i = 0; i < str.size(); i++){
+        int index = getIndex(str[i]);
+        if (i == 0){
+          result += Hat[index];
+        }else if (i == 1){
+          if (UpLeftArm == 1){
+            result += LeftArm[4] + "(" + LeftEye[index];
+          }else{
+          result += " (" + LeftEye[index];
+          }
+        }else if (i == 2){
+          result += Mouth[index];
+        }else if (i == 3) {
+          if (UpRightArm == 1){
+            result += RightEye[index] + ")" + RightArm[4] + "\n";
+          }else{
+            result += RightEye[index] + ")\n";
+          }
+        }else if (i == 4){
+          if (UpLeftArm != 1){
+            result += LeftArm[index];
+          }else{
+            result += " ";
+          }
+        }else if (i == 5){
+          if (UpRightArm != 1){
+            result += "(" + Torso[index] + ")";
+          }else{
+            result += "(" + Torso[index] + ")\n";
+          }
+        }else if (i == 6){
+          if (UpRightArm != 1){
+            result += RightArm[index] + "\n";
+          }
+        }
+        else if (i == 7){
+          result += Base[index];
+        }
+      }
+      return result;  
+    }
+    return "";
+  }
+};
+
+
+// int main(){
+
+//   cout << ariel::snowman(33232124);
+//   // int size = sizeof(CorrectNumbers)/sizeof(CorrectNumbers[0]);
+//   // cout << "size = " << size ;
+//   // string a = "";
+//   // // cout << "const int arr[] = {";
+//   // a +=  "const string snowman[] = {";
+//   // for (size_t i = 0; i < size; i++){
+//   //     // a +=  "\"" + nospaces(ariel::snowman(CorrectNumbers[i])) + "\"" + ",\n";
+//   //     a += "val = " ;
+//   //     a +=  "\n" ;
+//   //     a +=  "\"" + ariel::snowman(CorrectNumbers[i]) + "\"" + ",\n";
+//   //   // cout << ariel::snowman(CorrectNumbers[i]) << ",";
+//   // }
+//   // a +=   "};";
+//   // // cout << "}";
+//   // cout << a;
+//   return 0;
+// }
